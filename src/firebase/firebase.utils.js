d28eed4785530f9fa1e7c5c7c300350c266aa23c
extasy44 <seoheejun@gmail.com>
2020-04-30 22:37:10 +1000

    intergrated firebase into application, created sign in page with sign in component and sign in with google

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyBdSWm72v4ly2ppO_G0F2JK9mA8qXHAJE4",
+    authDomain: "crown-db-f1642.firebaseapp.com",
+    databaseURL: "https://crown-db-f1642.firebaseio.com",
+    projectId: "crown-db-f1642",
+    storageBucket: "crown-db-f1642.appspot.com",
+    messagingSenderId: "480265775401",
+    appId: "1:480265775401:web:584e1e8dc3f396c9236f38",
+    measurementId: "G-D8BWZ5N63X"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
---
+++ src/pages/sign-in/sign-in.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './sign-in.styles.scss';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SignInPage = () => (
+    <div>
+        <SignIn />
+    </div>
+)
+
+export default SignInPage;
