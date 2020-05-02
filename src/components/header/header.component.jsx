4043dbfc5337ba0da0cbab3a5d71de1228ccf977
extasy44 <seoheejun@gmail.com>
2020-05-02 22:30:30 +1000

    updated shop data with new data, updated cart utils to group items in cart

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -39,7 +39,7 @@ const Header = ( props ) => (
   </div>
 )
 
-const mapStateToProps = ({user:{currentUser}, hidden:{hidden}}) => ({
+const mapStateToProps = ({user:{currentUser}, cart:{hidden}}) => ({
   currentUser,
   hidden
 })
